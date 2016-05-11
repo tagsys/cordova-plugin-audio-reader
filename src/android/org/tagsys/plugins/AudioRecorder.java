71a29664fe335f8adfe2e56aa3ea0f204b568120
tagsys <young@tagsys.org>
2016-05-11 09:13:11 +0800

    Provide four primitives: create, start, stop and read

--- src/android/org/tagsys/plugins/AudioRecorder.java
+++ src/android/org/tagsys/plugins/AudioRecorder.java
@@ -4,10 +4,14 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONException;
 import android.media.MediaRecorder;
 import android.media.MediaPlayer;
 import android.media.AudioManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.os.CountDownTimer;
 import android.os.Environment;
 import android.content.Context;
@@ -15,104 +19,156 @@ import java.util.UUID;
 import java.io.FileInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import android.media.AudioTrack;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+
 
 public class AudioRecorder extends CordovaPlugin {
 
-  private MediaRecorder myRecorder;
-  private String outputFile;
-  private CountDownTimer countDowntimer;
+  private AudioRecord recorder;
+  private boolean stopped;
+  private int source;
+  private int sampleRate;
+  private int channel;
+  private int format;
+  private int bufferSize;
+  private int frameLength;
+  private LinkedBlockingQueue<short[]> blocks = new LinkedBlockingQueue<short[]>();
+  private JSONObject result = new JSONObject();
+
+  class AudioReader extends Thread{
+    AudioRecorder recorder;
+    public AudioReader(AudioRecorder recorder){
+      this.recorder = recorder;
+    }
+
+    @Override
+    public void run(){
+
+      while(recorder.stopped==false){
+        try{
+            Thread.sleep(10l);
+        } catch (InterruptedException e) {
+        }
+
+        try{
+          short[] block = new short[recorder.frameLength];
+          recorder.recorder.read(block, 0, recorder.frameLength);
+          if(recorder.blocks.size() > 4096){
+            recorder.blocks.take();
+          }
+          recorder.blocks.put(block);
+          // System.out.println("push a new block:"+recorder.blocks.size());
+        }catch(Exception ex){
+          ex.printStackTrace();
+        }
+      }
+    }
+  }
 
   @Override
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     Context context = cordova.getActivity().getApplicationContext();
-    Integer seconds;
-    if (args.length() >= 1) {
-      seconds = args.getInt(0);
-    } else {
-      seconds = 7;
+
+    if(action.equals("create")){
+        create(args.getString(0), callbackContext);
+        return true;
     }
-    if (action.equals("record")) {
-      outputFile = context.getFilesDir().getAbsoluteFile() + "/"
-        + UUID.randomUUID().toString() + ".m4a";
-      myRecorder = new MediaRecorder();
-      myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-      myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-      myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-      myRecorder.setAudioSamplingRate(44100);
-      myRecorder.setAudioChannels(1);
-      myRecorder.setAudioEncodingBitRate(32000);
-      myRecorder.setOutputFile(outputFile);
 
-      try {
-        myRecorder.prepare();
-        myRecorder.start();
-      } catch (final Exception e) {
-        cordova.getThreadPool().execute(new Runnable() {
-          public void run() {
-            callbackContext.error(e.getMessage());
-          }
-        });
-        return false;
-      }
-
-      countDowntimer = new CountDownTimer(seconds * 1000, 1000) {
-        public void onTick(long millisUntilFinished) {}
-        public void onFinish() {
-          stopRecord(callbackContext);
-        }
-      };
-      countDowntimer.start();
-      return true;
+    if(action.equals("start")){
+        start(callbackContext);
+        return true;
     }
 
     if (action.equals("stop")) {
-      countDowntimer.cancel();
-      stopRecord(callbackContext);
-      return true;
+        stop(callbackContext);
+        return true;
     }
 
-    if (action.equals("playback")) {
-      MediaPlayer mp = new MediaPlayer();
-      mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-      try {
-        FileInputStream fis = new FileInputStream(new File(outputFile));
-        mp.setDataSource(fis.getFD());
-      } catch (IllegalArgumentException e) {
-        e.printStackTrace();
-      } catch (SecurityException e) {
-        e.printStackTrace();
-      } catch (IllegalStateException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      try {
-        mp.prepare();
-      } catch (IllegalStateException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-        public void onCompletion(MediaPlayer mp) {
-          callbackContext.success("playbackComplete");
-        }
-      });
-      mp.start();
-      return true;
+    if(action.equals("read")){
+        read(callbackContext);
+        return true;
     }
 
     return false;
   }
 
-  private void stopRecord(final CallbackContext callbackContext) {
-    myRecorder.stop();
-    myRecorder.release();
-    cordova.getThreadPool().execute(new Runnable() {
-      public void run() {
-        callbackContext.success(outputFile);
+
+  private void create(String configStr,final CallbackContext callbackContext){
+
+    System.out.println("comeing to the create ufnction.");
+
+    try{
+      JSONObject config = new JSONObject(configStr);
+      this.source = config.getInt("source");
+      this.sampleRate = config.getInt("sampleRate");
+      this.channel = config.getInt("channel");
+      this.format = config.getInt("format");
+      this.bufferSize = 0;
+
+      if(channel==AudioFormat.CHANNEL_IN_MONO){
+        this.bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT) * 4;
+        this.frameLength = (int)(config.getDouble("frameLength"));
+      }else{
+        this.bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT) * 4;
+        this.frameLength = (int)(config.getDouble("frameLength")*2);
       }
-    });
+
+      this.recorder = new AudioRecord(source, sampleRate, channel, format, bufferSize);
+
+      callbackContext.success(0);
+
+    }catch(Exception ex){
+      ex.printStackTrace();
+      callbackContext.error(ex.getMessage());
+    }
+
+  }
+
+  private void start(final CallbackContext callbackContext){
+    try{
+      this.recorder.startRecording();
+      this.stopped = false;
+      new AudioReader(this).start();
+      callbackContext.success(0);
+    }catch(Exception ex){
+      ex.printStackTrace();
+      callbackContext.error(ex.getMessage());
+    }
+  }
+
+  private void stop(final CallbackContext callbackContext){
+    try{
+      this.recorder.stop();
+      this.recorder.release();
+      this.blocks.clear();
+      this.stopped = true;
+      callbackContext.success(0);
+    }catch(Exception ex){
+      ex.printStackTrace();
+      callbackContext.error(ex.getMessage());
+    }
+  }
+
+  private void read(final CallbackContext callbackContext){
+
+      try{
+        short[] block = this.blocks.poll();
+        if(block!=null){
+          result.put("error",0);
+          result.put("block", block);
+        }else{
+          result.put("error",1);
+          result.put("block",null);
+        }
+        callbackContext.success(result);
+      }catch(Exception ex){
+        ex.printStackTrace();
+        callbackContext.error(ex.getMessage());
+      }
   }
 
 }
--- www/AudioRecorder.js
+++ www/AudioRecorder.js
@@ -1,16 +1,79 @@
-function AudioRecorder() {
+
+var _extend = function(destination, source) {
+
+  return destination;
 }
 
-AudioRecorder.prototype.record = function (successCallback, errorCallback, duration) {
-  cordova.exec(successCallback, errorCallback, "AudioRecorder", "record", duration ? [duration] : []);
+/**Audio Source ***/
+var AudioSource = {
+  DEFAULT : 0,
+  MIC : 1,
+  VOICE_UPLINK : 2,
+  VOICE_DOWNLINK : 3,
+  VOICE_CALL : 4,
+  CAMCORDER : 5,
+  VOICE_RECOGNITION : 6,
+  VOICE_COMMUNICATION : 7,
+  REMOTE_SUBMIX : 8
+}
+
+/**Channel mode***/
+var AudioChannel={
+  CHANNEL_IN_MONO : 16,
+  CHANNEL_IN_STEREO : 12,
+}
+
+/***Audio format***/
+var AudioFormat = {
+  ENCODING_PCM_16BIT : 2,
+  ENCODING_PCM_8BIT : 3,
+  ENCODING_PCM_FLOAT : 4
+}
+
+/***Default config****/
+var DEFAULT_CONFIG = {
+    source: AudioSource.MIC,
+    channel: AudioChannel.CHANNEL_IN_MONO,
+    format: AudioFormat.ENCODING_PCM_16BIT,
+    sampleRate:44100,
+    frameLength:0.09*44100
+}
+
+var AudioRecorder = function(){
+
+  console.log('new audio recorder...');
+}
+
+AudioRecorder.prototype.create = function(successCallback, errorCallback, config){
+
+  console.log("create AudioRecorder");
+
+  var _config = DEFAULT_CONFIG;
+
+  if(config){
+    for (var property in config) {
+      _config[property] = config[property];
+    }
+  }
+
+  console.log("Audio recorder configuration:");
+
+  cordova.exec(successCallback, errorCallback, "AudioRecorder", "create", [_config]);
+
+}
+
+AudioRecorder.prototype.start = function (successCallback, errorCallback, config, duration) {
+  console.log("It is going to start.");
+  cordova.exec(successCallback, errorCallback, "AudioRecorder", "start", duration ? [duration] : []);
 };
 
 AudioRecorder.prototype.stop = function (successCallback, errorCallback) {
+  console.log("It is going to stop.");
   cordova.exec(successCallback, errorCallback, "AudioRecorder", "stop", []);
 };
 
-AudioRecorder.prototype.playback = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "AudioRecorder", "playback", []);
+AudioRecorder.prototype.read = function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "AudioRecorder", "read", []);
 };
 
 AudioRecorder.install = function () {
@@ -18,6 +81,7 @@ AudioRecorder.install = function () {
     window.plugins = {};
   }
   window.plugins.audioRecorder = new AudioRecorder();
+
   return window.plugins.audioRecorder;
 };
 
